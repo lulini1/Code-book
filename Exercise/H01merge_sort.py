68916cf0e610d94d75e80e63061604e62f5bef1b
lulini1 <lu1729374743@gmail.com>
2020-06-30 07:24:55 +0800

    codebook

--- Exercise/H01merge_sort.py
+++ Exercise/H01merge_sort.py
@@ -1,20 +1,20 @@
-#猜数字游戏
+#merge sort
+#归并排序
 import random
 
-secret = random.randint(1,100)
-print('''猜数游戏！
-我想了一个1-100的整数，你最多可以猜6次，
-看看能猜出来吗？''')
-tries = 1
-while tries <= 6:
-    guess = int (input("1-100的整数，第%d次猜，请输入："%(tries,)))
-    if guess == secret:
-        print("恭喜答对了！你只猜了%d次\n就是这个:%d!"% (tries,secret))
-        break
-    elif guess > secret:
-        print("不好意思，你的数大了一点！")
-    else:
-        print("不好意思，你的数大了一点！")
-    tries += 1
-else:
-    print("哎呀！怎么也没猜中！再见！")
+
+def merge_sort(data_list):
+    if len(data_list) <= 1:
+        return data_list
+    middle = int(len(data_list) / 2)
+    left = merge_sort(data_list[:middle])
+    right = merge_sort(data_list[middle:])
+    merged = []
+    while left and right:
+        merged.append(left.pop(0) if left[0] <= right[0] else right.pop(0))
+    merged.extend(right if right else left)
+    return merged
+
+
+data_list = [random.randint(1,100) for _ in range(50)]
+print(merge_sort(data_list))
